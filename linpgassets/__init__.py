dae49bc1615705ed3053afdce29aa5dc65512bd2
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2023-10-17 15:33:55 -0700

    update build environment

--- linpgassets/__init__.py
+++ linpgassets/__init__.py
@@ -1,7 +1,9 @@
 from os import path as PATH
+from typing import Final
 
 # 引擎素材路径
-__LINPG_INTERNAL_IMAGE_PATH: str = PATH.join(PATH.dirname(__file__), "image")
+__LINPG_INTERNAL_IMAGE_PATH: Final[str] = PATH.join(PATH.dirname(__file__), "image")
+
 
 # 获取引擎素材路径
 def get_image_location() -> str:
@@ -9,7 +11,10 @@ def get_image_location() -> str:
 
 
 # 数据库路径
-__DATABASE_PATH: str = PATH.join(PATH.dirname(__file__), "config", "database.json")
+__DATABASE_PATH: Final[str] = PATH.join(
+    PATH.dirname(__file__), "config", "database.json"
+)
+
 
 # 获取数据库路径
 def get_database_path() -> str:
