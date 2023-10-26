7aa0d9ec390305f0f9fff47a67357a9806fcefc1
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2023-10-26 12:05:27 -0700

    add support for getting database directly

--- linpgassets/__init__.py
+++ linpgassets/__init__.py
@@ -1,3 +1,4 @@
+from json import load as JSON_LOAD
 from os import path as PATH
 from typing import Final
 
@@ -10,12 +11,12 @@ def get_image_location() -> str:
     return __LINPG_INTERNAL_IMAGE_PATH
 
 
-# 数据库路径
-__DATABASE_PATH: Final[str] = PATH.join(
-    PATH.dirname(__file__), "config", "database.json"
-)
-
-
 # 获取数据库路径
 def get_database_path() -> str:
-    return __DATABASE_PATH
+    return PATH.join(PATH.dirname(__file__), "config", "database.json")
+
+
+# 获取数据库
+def get_database() -> dict[str, dict[str, dict[str, bool]]]:
+    with open(get_database_path(), "r", encoding="utf-8") as f:
+        return dict(JSON_LOAD(f))
